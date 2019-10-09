742d636df86eb035b5a7874933528a0b2afe8607
Khasiano <NewbieCoder10>
2019-10-09 13:10:31 -0500

    Updated buttons and inserted new things.

--- app/src/main/java/edu/cnm/deepdive/blackjack/controller/PlayerHandFragment.java
+++ app/src/main/java/edu/cnm/deepdive/blackjack/controller/PlayerHandFragment.java
@@ -1,17 +1,29 @@
 package edu.cnm.deepdive.blackjack.controller;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.ViewModelProviders;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import edu.cnm.deepdive.blackjack.R;
 import edu.cnm.deepdive.blackjack.model.pojo.HandWithCards;
 import edu.cnm.deepdive.blackjack.viewmodel.MainViewModel;
 
 public class PlayerHandFragment extends HandFragment {
 
+  @Override
+  public View onCreateView(LayoutInflater inflater, ViewGroup container,
+      Bundle savedInstanceState) {
+    View view = super.onCreateView(inflater, container, savedInstanceState);
+    FloatingActionButton hitMe = view.findViewById(R.id.hit_me);
+    hitMe.setOnClickListener((v) -> getViewModel().hitPlayer());
+    return view;
+  }
 
   @Override
   public LiveData<HandWithCards> handToObserve(MainViewModel viewModel) {
